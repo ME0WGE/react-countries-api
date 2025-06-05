b41e75d2f38398511441e8447b938b4fe61e69db
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-05 10:29:25 +0200

    add App.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "../Layout";
 import "./App.css";
+import CountryDetails from "./pages/CountryDetails/CountryDetails";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
-      <></>
+      <Routes>
+        {/* HOMEPAGE */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/country/" element={<CountryDetails />} />
+        </Route>
+      </Routes>
     </>
   );
 }
