b5098566fd94bc31d84bc95bf9a273f5cabfa85c
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-05 09:49:30 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* Ready to code! */}
+      {/* Script written by github.com/ME0WGE */}
+    </>
+  );
+}
+
+export default App;
