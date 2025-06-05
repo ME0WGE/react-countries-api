17ef98b443ef96a3e64751025b9a5e6733159782
molengeek <saoussan.elk@gmail.com>
2025-06-05 14:02:23 +0200

    finSaoussan

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -1,5 +1,35 @@
 import "./CountryCard.css";
+import { useEffect, useState } from "react";
 
-export default function CountryCard() {
-  return;
+
+export default function CountryCard({countries = []}) {
+  if (!countries.length) {
+    return <div>Loading...</div>;
+  } 
+
+
+  return(
+
+
+    <div className="divCardGlo">
+      {countries.map((country) => (
+
+        <div key={country.name.common} className="divCardOne">
+          <div className="cardImg">
+            <img src={country.flags.svg} alt={country.flags.alt} />
+          </div>
+          <div className="cardText">
+
+            <h4>{country.name.common}</h4>
+            <p><b>Population: </b>{country.population}</p>
+            <p><b>Région: </b>{country.region}</p>
+            <p><b>Capitale: </b>{country.capital}</p>
+          </div>
+
+        </div>
+      ))}
+
+
+    </div>
+  )
 }
