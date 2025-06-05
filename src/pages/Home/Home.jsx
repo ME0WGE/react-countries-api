9025d7fc189a0341bdf64936d110d65d043e9c2b
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-05 11:39:44 +0200

    searchbar okayge

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
 import "./Home.css";
 
 export default function Home() {
-  return;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <>
+      <div className="search-filter">
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </>
+  );
 }
