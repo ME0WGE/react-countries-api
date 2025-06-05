19f6cf4424ebade352c84e474eb38cbb37b7256d
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-05 14:05:39 +0200

    fix

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -1,35 +1,34 @@
 import "./CountryCard.css";
-import { useEffect, useState } from "react";
 
-
-export default function CountryCard({countries = []}) {
+export default function CountryCard({ countries = [] }) {
   if (!countries.length) {
     return <div>Loading...</div>;
-  } 
+  }
 
-
-  return(
-
-
+  return (
     <div className="divCardGlo">
       {countries.map((country) => (
-
         <div key={country.name.common} className="divCardOne">
           <div className="cardImg">
             <img src={country.flags.svg} alt={country.flags.alt} />
           </div>
           <div className="cardText">
-
             <h4>{country.name.common}</h4>
-            <p><b>Population: </b>{country.population}</p>
-            <p><b>Région: </b>{country.region}</p>
-            <p><b>Capitale: </b>{country.capital}</p>
+            <p>
+              <b>Population: </b>
+              {country.population}
+            </p>
+            <p>
+              <b>Région: </b>
+              {country.region}
+            </p>
+            <p>
+              <b>Capitale: </b>
+              {country.capital}
+            </p>
           </div>
-
         </div>
       ))}
-
-
     </div>
-  )
+  );
 }
